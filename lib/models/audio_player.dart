4257ae48ecc1b68a94410c55a00f6ff039e5e09a
Jacob <hortonjacob1@gmail.com>
2024-09-05 11:12:28 +0100

    chore: dump deps + fix bug

--- lib/models/audio_player.dart
+++ lib/models/audio_player.dart
@@ -19,7 +19,7 @@ class AudioPlayerModel extends ChangeNotifier {
     if (_currentTrack != track) {
       await _player.setAudioSource(
         AudioSource.uri(
-          Uri.parse('asset:///tracks/$track'),
+          Uri.parse('asset:///assets/tracks/$track'),
           tag: MediaItem(
             id: track,
             title: formatFilename(track),
